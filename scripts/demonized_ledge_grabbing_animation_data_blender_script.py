298daf53d10397de434dff9b713a03bcd1d0061f
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2023-10-02 20:03:45 +1000

    animation export from blender

---
+++ scripts/demonized_ledge_grabbing_animation_data_blender_script.py
@@ -0,0 +1,72 @@
+# Python script for export anim data in blender
+# Paste in Blender scripting window
+
+import bpy
+
+context = bpy.context
+obj = context.object
+action =  context.object.animation_data.action
+print(context)
+print(obj)
+print(action)
+indexToAxis = {
+    0: 'X',
+    1: 'Y',
+    2: 'Z',
+}
+
+length = context.scene.frame_end
+fps = context.scene.render.fps
+animTime = length / fps 
+curves = {}
+for fcurve in action.fcurves:
+    i = 0
+    time = 0
+    curve = {}
+    p1 = None
+    p2 = None
+    p3 = None
+    p4 = None
+    for kfp in fcurve.keyframe_points:
+        # Get control point
+        p1 = [kfp.co[0], kfp.co[1]]
+        
+        # Get right handle
+        p2 = [kfp.handle_right[0], kfp.handle_right[1]]
+        
+        # Get next control point left handle
+        nextIndex = i + 1
+        nextKfp = None
+        if nextIndex < len(fcurve.keyframe_points):
+            nextKfp = fcurve.keyframe_points[nextIndex]
+            p3 = [nextKfp.handle_left[0], nextKfp.handle_left[1]]
+        
+            # Get next control point
+            p4 = [nextKfp.co[0], nextKfp.co[1]]
+        else:
+            p1 = [kfp.co[0], kfp.co[1]]
+            p2 = [length, kfp.co[1]]
+            p3 = [length, kfp.co[1]]
+            p4 = [length, kfp.co[1]]
+        curve[p1[0] / fps] = [p1, p2, p3, p4]
+        i = i + 1
+    curve[animTime] = [p4, p4, p4, p4]
+    curves[fcurve.data_path + indexToAxis[fcurve.array_index]] = curve
+
+formattedCurves = ""
+for curve in curves:
+    s = f"{curve} = {{"
+    for time in curves[curve]:
+        data = curves[curve][time]
+        s += "\n\t\t[%s] = %s" % (time, f"{{ vector():set({data[0][0]}, {data[0][1]}, 0), vector():set({data[1][0]}, {data[1][1]}, 0), vector():set({data[2][0]}, {data[2][1]}, 0), vector():set({data[3][0]}, {data[3][1]}, 0), }},")
+    formattedCurves += s + "\n\t},\n\t"
+print(f"""
+data = {{
+    animationFrameLength = {length},
+    originalFps = {fps},
+    calculatedTime = {animTime},
+    curves = {{
+        {formattedCurves}      
+    }}
+}}    
+""") 
